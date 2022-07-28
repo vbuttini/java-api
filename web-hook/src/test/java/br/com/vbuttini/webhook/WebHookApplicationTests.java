7e1aefd30692b0e85b88ee54b3b382fb61d4e97e
vbuttini <vbuttini22@gmail.com>
2022-07-28 10:33:49 -0300

    first commit

---
+++ web-hook/src/test/java/br/com/vbuttini/webhook/WebHookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vbuttini.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebHookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
